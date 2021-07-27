4a2742a18be0e9ebc5f131907ea717607449faf6
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2021-07-28 01:30:48 +0900

    config

--- src/views/Youtube.vue
+++ src/views/Youtube.vue
@@ -1,6 +1,7 @@
 <template>
+  <div class="youtube">
   <div class="row">
-    <div class="col-lg-3 col-md-6"  :key="i" v-for="(product, i) in list">
+    <div class="col-lg-3 col-md-6 w-50"  :key="i" v-for="(product, i) in list">
           <div class="mb-4 card"><a v-bind:href="'https://youtu.be/'+ product.id"><img class="card-img-top" v-bind:src="product.snippet.thumbnails.high.url" alt="Card image cap"/></a>
             <div class="card-body">
               <div class="card-title h5">{{ product.snippet.title }}</div>
@@ -11,6 +12,7 @@
             </div>
           </div>
         </div>
+  </div>
   </div>
 </template>
 
